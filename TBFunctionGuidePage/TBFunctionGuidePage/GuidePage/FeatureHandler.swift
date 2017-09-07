29de4d071a88c9bb2e3839f3b35c29b89128ae2f
魏武涛 <mac@jingtaoMac.local>
2017-09-07 22:01:19 +0800

    feat: 基本思路解决

---
+++ TBFunctionGuidePage/TBFunctionGuidePage/GuidePage/FeatureHandler.swift
@@ -0,0 +1,13 @@
+//
+//  FeatureHandler.swift
+//  TBFunctionGuidePage
+//
+//  Created by Mac on 2017/9/7.
+//  Copyright © 2017年 LiYou. All rights reserved.
+//
+
+import UIKit
+
+class FeatureHandler: NSObject {
+
+}
---
+++ TBFunctionGuidePage/TBFunctionGuidePage/GuidePage/GuidePageView.swift
@@ -0,0 +1,13 @@
+//
+//  GuidePageView.swift
+//  TBFunctionGuidePage
+//
+//  Created by Mac on 2017/9/7.
+//  Copyright © 2017年 LiYou. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+}
