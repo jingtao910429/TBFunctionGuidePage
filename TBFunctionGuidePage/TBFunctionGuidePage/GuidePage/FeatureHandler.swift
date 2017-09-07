93d003276f241337d238164783603b5884908961
魏武涛 <mac@jingtaoMac.local>
2017-09-07 22:08:24 +0800

    fix: item配置添加

--- TBFunctionGuidePage/TBFunctionGuidePage/GuidePage/FeatureHandler.swift
+++ TBFunctionGuidePage/TBFunctionGuidePage/GuidePage/FeatureHandler.swift
@@ -8,6 +8,21 @@
 
 import UIKit
 
-class FeatureHandler: NSObject {
+//单布局元素在界面上垂直居中时，是将介绍文案布局顶部，还是底部
+enum AlignmentPriority {
+    //顶部优先
+    case top
+    //底部优先
+    case bottom
+}
 
+class FeatureHandler: NSObject {
+    //高亮元素
+    public var focusView: UIView?
+    //高亮元素frame
+    public var focusFrame: CGRect?
+    //高亮元素圆角半径
+    public var focusCornerRadius: CGFloat = 0
+    //高亮元素Insets
+    public var focusInsets: UIEdgeInsets = UIEdgeInsets()
 }
